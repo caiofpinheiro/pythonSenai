d78f462e75a841c7efc8693a3b86c49eeb110d76
Caio Pinheiro <86423541+caiofpinheiro@users.noreply.github.com>
2024-04-09 19:19:28 +0000

    Exercicios Python

---
+++ Exercícios.py/6 - operaçãoLogica.py
@@ -0,0 +1,29 @@
+import os
+
+os.system("clear")
+
+numero1: int; numero2: int; resultado: int
+operacao: str
+
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+operacao = str(input("Digite a operação(*+-/): "))
+
+if operacao == "*":
+    resultado = numero1 * numero2
+    
+elif operacao == "+":
+    resultado = numero1 + numero2
+
+elif operacao == "-":
+    resultado = numero1 - numero2
+
+elif operacao == "/":
+    resultado = numero1 / numero2
+
+print(f"Resultado: {resultado}")
+
+
+
+    
