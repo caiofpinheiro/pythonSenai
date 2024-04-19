f03fe8288ca7aa7df3d9150d96464e3e55512481
Caio Pinheiro <86423541+caiofpinheiro@users.noreply.github.com>
2024-04-19 18:53:29 +0000

    Exercicios Python

--- Vetores.py/5 - somaPositivoQtdNegativo.py
+++ Vetores.py/5 - somaPositivoQtdNegativo.py
@@ -5,7 +5,7 @@ os.system("clear")
 numReais =[]
 somaPos = 0
 qtdNegat = 0
-contador = 0
+contadorNeg = 0
 
 for i in range(1,11):
     numeros = float(input("Digite os números: "))
@@ -16,9 +16,9 @@ for i in range(len(numReais)):
         somaPos += numReais[i]
     
     if numReais[i] < 0:
-        contador += 1
+        contadorNeg += 1
 
 os.system("clear")
 
-print(f"Quantidade de negativos: {contador}")
+print(f"Quantidade de negativos: {contadorNeg}")
 print(f"Soma dos positivos: {somaPos}")
