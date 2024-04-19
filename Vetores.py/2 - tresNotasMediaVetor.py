d7fa3c8508ac41ce9315c2869e5c7f5dc71e140f
Caio Pinheiro <86423541+caiofpinheiro@users.noreply.github.com>
2024-04-19 16:55:16 +0000

    Exercicios Python

---
+++ Vetores.py/2 - tresNotasMediaVetor.py
@@ -0,0 +1,23 @@
+import os
+
+os.system("clear")
+
+notas =[]
+contador = 0
+soma = 0
+
+for i in range(3):
+     nota = float(input(f"Digite a nota de número {i + 1}: "))
+     notas.append(nota)
+
+
+os.system("clear")
+
+for i in range(len(notas)):
+     print(f"{i+1}ª Nota: {notas[i]}")
+     soma += notas[i]
+     contador += 1
+
+media = soma / contador
+
+print(f"Média das notas: {media}")
